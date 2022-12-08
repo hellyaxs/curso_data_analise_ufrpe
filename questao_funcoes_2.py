314b3666db14e33d4119dd823ad37af9951d4430
hellyaxs <eliasvitor.dev@gmail.com>
2022-12-08 18:22:34 -0300

    questão funcao 2 ok

--- questao_funcoes_2.py
+++ questao_funcoes_2.py
@@ -1,1 +1,23 @@
-{\rtf1}
+def valorPagamento(valor,atraso):
+    if(atraso == 0):
+        return valor
+    else:
+        valor *= 1.03
+        valor = valor* (1+0.1)**atraso 
+        return valor
+
+
+valor= 1
+atraso =0
+varTotal =0
+while (valor!= 0):
+    valor = int(input("digite o valor a ser pago: "))
+    if(valor == 0):
+        break
+    atraso = int(input("digite o numero de dias de atraso: "))
+    varTotal +=valorPagamento(valor,atraso)
+    print("o novo valor a ser pago é: ",valorPagamento(valor,atraso))
+
+
+print("o valo total a ser pago e:",varTotal)    
+
